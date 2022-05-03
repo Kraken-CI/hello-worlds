feb5cb4a14bb1401cb0c7983f439b886eb3f27c7
Michal Nowikowski <godfryd@gmail.com>
2022-05-03 14:42:16 +0200

    added tracing program code

--- run.sh
+++ run.sh
@@ -6,23 +6,23 @@ lang=$1
 
 case $lang in
 
-    C)       cd $lang; make run;;
-    C++)     cd $lang; make run;;
-    C#)      cd $lang; make run;;
-    Clojure) cd $lang; make run;;
-    Elixir)  cd $lang; make run;;
-    Erlang)  cd $lang; make run;;
-    Go)      cd $lang; make run;;
-    Groovy)  cd $lang; make run;;
-    Haxe)    cd $lang; make run;;
-    Java)    cd $lang; make run;;
-    Julia)   cd $lang; make run;;
-    Perl)    cd $lang; make run;;
-    PHP)     cd $lang; make run;;
-    Python)  cd $lang; make run;;
-    Ruby)    cd $lang; make run;;
-    Rust)    cd $lang; make run;;
-    Swift)   cd $lang; make run;;
+    C)       cd $lang; make all;;
+    C++)     cd $lang; make all;;
+    C#)      cd $lang; make all;;
+    Clojure) cd $lang; make all;;
+    Elixir)  cd $lang; make all;;
+    Erlang)  cd $lang; make all;;
+    Go)      cd $lang; make all;;
+    Groovy)  cd $lang; make all;;
+    Haxe)    cd $lang; make all;;
+    Java)    cd $lang; make all;;
+    Julia)   cd $lang; make all;;
+    Perl)    cd $lang; make all;;
+    PHP)     cd $lang; make all;;
+    Python)  cd $lang; make all;;
+    Ruby)    cd $lang; make all;;
+    Rust)    cd $lang; make all;;
+    Swift)   cd $lang; make all;;
 
     "")    echo "help: invoke ./run.sh <lang-folder>";;
     *)     echo "unsupported lang $lang";;
