cef0e9d3d2f21ad1732dd0de4d3cca005d8a0432
Michal Nowikowski <godfryd@gmail.com>
2022-05-03 06:35:13 +0200

    added Go to run.sh

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ case $lang in
 
     C)     cd $lang; make run;;
     C++)   cd $lang; make run;;
+    Go)    cd $lang; make run;;
 
     "")    echo "help: invoke ./run.sh <lang-folder>";;
     *)     echo "unsupported lang $lang";;
