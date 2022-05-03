3d0ec107797b45fb4df672e1836c38b4040a0540
Michal Nowikowski <godfryd@gmail.com>
2022-05-03 06:56:16 +0200

    added kraken workflow

---
+++ kraken-workflow.py
@@ -0,0 +1,39 @@
+def stage(ctx):
+    langs = [
+        ('C', 'gcc:11.3'),
+        ('C++', 'gcc:11.3'),
+        ('Go', 'golang:1.18'),
+        ('Java', 'openjdk:11'),
+    ]
+
+    jobs = []
+
+    for lang_name, lang_image in langs:
+        jobs.append({
+            "name": "hello world %s" % lang_name,
+            "steps": [{
+            "tool": "git",
+                "checkout": "https://github.com/Kraken-CI/hello-worlds.git",
+                "branch": "main"
+            }, {
+                "tool": "shell",
+                "cmd": "./run.sh %s" % lang_name,
+                "cwd": "hello-worlds"
+            }],
+            "environments": [{
+                "executor": "docker",
+                "system": "krakenci/%s" % lang_image,
+                "agents_group": "all",
+                "config": "default"
+            }]
+        })
+
+    return {
+        "parent": "root",
+        "triggers": {
+            "parent": True,
+        },
+        "parameters": [],
+        "configs": [],
+        "jobs": jobs
+    }
